9bac721502b4b29d651979f496c7e26379dfcbe4
kyawphyothu <dev@kyawphyothu.com>
2026-03-14 19:38:52 +0700

    refactor: simplify next.config.ts by removing commented-out configurations

--- next.config.ts
+++ next.config.ts
@@ -9,28 +9,7 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  // output: 'export', // This enables static HTML export
-  // images: {
-  //   unoptimized: true, // For static export, we need this for images
-  //   // Add domains if you have external images
-  //   domains: ['https://kyawphyothu.com'],
-  // },
-  // Any other existing config...
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export', // This enables static HTML export
-//     images: {
-//       unoptimized: true, // For static export, we need this for images
-//       // Add domains if you have external images
-//       domains: ['example.com'],
-//     },
-//     // Any other existing config...
-//   }
-
-//   module.exports = nextConfig
